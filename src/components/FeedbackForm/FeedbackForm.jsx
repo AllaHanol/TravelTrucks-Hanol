9778cd5cdfb4f942f19db54d1db73c3bd1f00f24
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2025-01-17 20:44:35 +0200

    zuruck zum started form

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -5,15 +5,12 @@ import {
   ErrorMessage,
 } from "formik";
 import {
-  // ToastContainer,
+  ToastContainer,
   toast,
 } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useId } from "react";
 import * as Yup from "yup";
-
-import Flatpickr from "react-flatpickr";
-import "flatpickr/dist/flatpickr.css";
 
 import css from "./FeedbackForm.module.css";
 import Button from "../Button/Button.jsx";
@@ -26,18 +23,10 @@ const FeedbackSchema =
       .required("Required"),
     email: Yup.string()
       .email("Must be a valid email!")
-      .required("Email is required!"),
-    date: Yup.date().required(
+      .required("Required"),
+    date: Yup.string().required(
       "Required",
     ),
-    bookingDate: Yup.string()
-      .matches(
-        /^(\d{2})\.(\d{2})\.(\d{4})$/,
-        "Date must be in format dd.mm.yyyy",
-      )
-      .required(
-        "Select date in the future",
-      ),
     message: Yup.string()
       .min(3, "Too short")
       .max(256, "Too long"),
@@ -68,171 +57,102 @@ export default function FeedbackForm() {
   };
 
   return (
-    // <section
-    //   className={css.form_section}
-    // >
-    //   <ToastContainer />
-    //   <div className={css.title}>
-    //     <h3>Book your campervan now</h3>
-    //     <p>
-    //       Stay connected! We are always
-    //       ready to help you.
-    //     </p>
-    //   </div>
-    <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={FeedbackSchema}
+    <section
+      className={css.form_section}
     >
-      {({ setFieldValue }) => (
-        <div
-          className={css.form_section}
-        >
-          <h3 className={css.title}>
-            Book your campervan now
-          </h3>
-          <p>
-            Stay connected! We are
-            always ready to help you.
-          </p>
-          <Form className={css.form}>
-            <div>
-              <label
-                htmlFor={nameFieldId}
-              ></label>
-              <Field
-                type="text"
-                name="name"
-                id={nameFieldId}
-                placeholder="Name*"
-                className={css.input}
-              />
-              <ErrorMessage
-                name="name"
-                component="span"
-              />
-            </div>
+      <ToastContainer />
+      <div className={css.title}>
+        <h3>Book your campervan now</h3>
+        <p>
+          Stay connected! We are always
+          ready to help you.
+        </p>
+      </div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={
+          FeedbackSchema
+        }
+      >
+        <Form className={css.form}>
+          <div>
+            <label
+              htmlFor={nameFieldId}
+            ></label>
+            <Field
+              type="text"
+              name="name"
+              id={nameFieldId}
+              placeholder="Name*"
+              className={css.input}
+            />
+            <ErrorMessage
+              name="name"
+              component="span"
+            />
+          </div>
 
-            <div>
-              <label
-                htmlFor={emailFieldId}
-              ></label>
-              <Field
-                type="email"
-                name="email"
-                id={emailFieldId}
-                placeholder="Email*"
-                className={css.input}
-              />
-              <ErrorMessage
-                name="email"
-                component="span"
-              />
-            </div>
+          <div>
+            <label
+              htmlFor={emailFieldId}
+            ></label>
+            <Field
+              type="email"
+              name="email"
+              id={emailFieldId}
+              placeholder="Email*"
+              className={css.input}
+            />
+            <ErrorMessage
+              name="email"
+              component="span"
+            />
+          </div>
 
-            <div
-              className={
-                css.groupFieldAndError
-              }
-            >
-              <Flatpickr
-                placeholder="Booking date*"
-                className={css.input}
-                id={dateFieldId}
-                options={{
-                  dateFormat: "d.m.Y",
-                  minDate: new Date(),
-                }}
-                onChange={(
-                  selectedDates,
-                ) => {
-                  if (
-                    selectedDates.length >
-                    0
-                  ) {
-                    const formattedDate =
-                      selectedDates[0]
-                        .toLocaleDateString(
-                          "en-GB",
-                        )
-                        .replace(
-                          /\//g,
-                          ".",
-                        );
-                    setFieldValue(
-                      "bookingDate",
-                      formattedDate,
-                    );
-                  }
-                }}
-                onInput={(e) => {
-                  setFieldValue(
-                    "bookingDate",
-                    e.target.value,
-                  );
-                }}
-              />
-              <ErrorMessage
-                name="bookingDate"
-                component="span"
-                className={css.error}
-              />
-            </div>
-
-            {/* <div> */}
-            {/* <label
+          <div>
+            <label
               htmlFor={dateFieldId}
             ></label>
             <Field
               type="text"
               name="date"
               id={dateFieldId}
-              className={css.input}
-              onFocus={(e) =>
-                (e.target.placeholder =
-                  "")
-              }
-              onBlur={(e) =>
-                (e.target.placeholder =
-                  "Booking date*")
-              }
               placeholder="Booking date*"
+              className={css.input}
             />
-
             <ErrorMessage
               name="date"
               component="span"
-            /> */}
-            {/* </div> */}
+            />
+          </div>
 
-            <div>
-              <label
-                htmlFor={msgFieldId}
-              ></label>
-              <Field
-                as="textarea"
-                name="message"
-                id={msgFieldId}
-                rows="5"
-                placeholder="Comment"
-                className={`${css.textarea} ${css.input}`}
-              />
-              <ErrorMessage
-                name="message"
-                component="span"
-              />
-            </div>
+          <div>
+            <label
+              htmlFor={msgFieldId}
+            ></label>
+            <Field
+              as="textarea"
+              name="message"
+              id={msgFieldId}
+              rows="5"
+              placeholder="Comment"
+              className={`${css.textarea} ${css.input}`}
+            />
+            <ErrorMessage
+              name="message"
+              component="span"
+            />
+          </div>
 
-            <Button
-              type="submit"
-              variant="submit"
-            >
-              Submit
-            </Button>
-          </Form>
-        </div>
-      )}
-    </Formik>
-    // </section>
+          <Button
+            type="submit"
+            variant="submit"
+          >
+            Submit
+          </Button>
+        </Form>
+      </Formik>
+    </section>
   );
 }
